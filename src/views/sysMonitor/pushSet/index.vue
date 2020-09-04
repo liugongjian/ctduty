294ffef4554bf9646f4dd9b8ba5f759648e6a840
杨宁 <selina123yang@126.com>
2020-09-04 09:44:52 +0800

    推送设置

--- src/views/sysMonitor/pushSet/index.vue
+++ src/views/sysMonitor/pushSet/index.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="push">
     推送设置
+
+    <el-form ref="form" :model="form" label-width="80px">
+
+      <el-form-item label="开始时间">
+        <el-col :span="11">
+          <el-time-picker
+            v-model="form.date1"
+            :picker-options="{
+              selectableRange:'00:00:00 -23:59:00'
+            }"
+            format="HH:mm"
+            value-format="HH:mm"
+
+            placeholder="选择时间">
+          </el-time-picker>
+        </el-col>
+      </el-form-item>
+
+      <el-form-item label="结束时间">
+        <el-col :span="11">
+          <el-time-picker
+            v-model="form.date2"
+            :picker-options="{
+              selectableRange: form.date1+ ':00' + '- 23:59:00'
+            }"
+            format="HH:mm"
+            value-format="HH:mm"
+            placeholder="选择时间">
+          </el-time-picker>
+        </el-col>
+      </el-form-item>
+
+      <el-form-item label="弹窗推送服务">
+        <el-switch v-model="form.deliveryPush"></el-switch>
+      </el-form-item>
+      <el-form-item label="短信服务">
+        <el-switch v-model="form.deliveryMessage"></el-switch>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      form: {
+        date1: '',
+        date2: '',
+        deliveryPush: false,
+        deliveryMessage: false
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.el-form {
+  width: 100%;
+}
 </style>
