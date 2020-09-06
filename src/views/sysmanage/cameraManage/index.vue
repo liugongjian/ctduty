2b1db837466ffd8b0186ffb859612cbe9211f912
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-09-06 15:04:26 +0800

    全屏功能

--- src/views/sysmanage/cameraManage/index.vue
+++ src/views/sysmanage/cameraManage/index.vue
@@ -76,13 +76,13 @@
                     <div style=" word-wrap: break-word">{{ form.id }}</div>
                   </el-form-item>
                   <el-form-item label="负责人：">
-                    <div style=" word-wrap: break-word">{{ form.inCharge }}</div>
+                    <div style=" word-wrap: break-word">{{ form.inCharge.username }}</div>
                   </el-form-item>
                   <el-form-item label="经度信息：">
-                    <div style=" word-wrap: break-word">{{ form.longitude }}</div>
+                    <div style=" word-wrap: break-word">{{ form.longitude.toFixed(2) }}</div>
                   </el-form-item>
                   <el-form-item label="纬度信息：">
-                    <div style=" word-wrap: break-word">{{ form.latitude }}</div>
+                    <div style=" word-wrap: break-word">{{ form.latitude.toFixed(2) }}</div>
                   </el-form-item>
                   <el-form-item label="地址：">
                     <div style=" word-wrap: break-word">{{ form.address }}</div>
@@ -107,7 +107,7 @@
                 <el-form :model="editForm" label-position="right" label-width="100px">
                   <el-form-item label="摄像头ID："><el-input v-model="editForm.id" placeholder="请输入摄像头ID" class="filter-item" style="width: 300px;"></el-input>
                   </el-form-item>
-                  <el-form-item label="负责人："><el-input v-model="editForm.inCharge" placeholder="请输入负责人" class="filter-item" style="width: 300px;"></el-input>
+                  <el-form-item label="负责人："><el-input v-model="editForm.inCharge.username" placeholder="请输入负责人" class="filter-item" style="width: 300px;"></el-input>
                   </el-form-item>
                   <el-form-item label="摄像头经度："><el-input v-model="editForm.longitude" placeholder="请输入摄像头经度" class="filter-item" style="width: 300px;"></el-input>
                   </el-form-item>
@@ -141,10 +141,11 @@
 <script>
 import VueAMap from 'vue-amap'
 import CameraList from './list.vue'
+import moment from 'moment'
 import { Alert } from 'element-ui'
 import addSvg from '../../../icons/svg/address.svg'
 import {
-  fetchAllCameraList, editCamera
+  fetchAllCameraList, editCamera, addCamera
 } from '@/api/camera'
 const amapManager = new VueAMap.AMapManager()
 export default {
@@ -244,8 +245,8 @@ export default {
       showZwMes: true,
       typeOptions: [{ name: '地图模式', _id: 'map' },
         { name: '列表模式', _id: 'list' }],
-      zoom: 12,
-      center: [110.09, 34.58],
+      zoom: 10,
+      center: [109.5, 34.5],
       dialogVisable: false,
       markersDom: null,
       markers: [],
@@ -262,69 +263,10 @@ export default {
       }
     }
   },
+  created() {
+    this.getList()
+  },
   mounted() {
-    setTimeout(() => {
-      this.formInfo = [
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.09,
-          latitude: 34.58,
-          address: '张三家',
-          url: '哈哈',
-          name: '张三',
-          createTime: '2020-09-10',
-          cl: 0 },
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.088,
-          latitude: 34.56,
-          address: '李四门口',
-          name: '李四',
-          createTime: '2020-09-10',
-          url: '哈哈',
-          cl: 0 },
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.093,
-          latitude: 34.66,
-          address: '王五家',
-          name: '李四',
-          createTime: '2020-09-10',
-          url: '哈哈',
-          cl: 0 },
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.074,
-          latitude: 34.42,
-          address: '王二',
-          name: '李四',
-          createTime: '2020-09-10',
-          url: '哈哈',
-          cl: 0 },
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.034,
-          latitude: 34.56,
-          address: '小卖部',
-          name: '李四',
-          createTime: '2020-09-10',
-          url: '哈哈',
-          cl: 0 },
-        { id: '567',
-          inCharge: 'safsafjk',
-          longitude: 110.09,
-          latitude: 34.58,
-          address: '村委会',
-          name: '李四',
-          createTime: '2020-09-10',
-          url: '哈哈',
-          cl: 0 }
-      ]
-      this.formInfo.forEach(item => {
-        this.markers.push({ position: [item.longitude, item.latitude], /* content: `<img class='markerImg' data=${JSON.stringify(item)} src="https://webapi.amap.com/theme/v1.3/markers/b/mark_bs.png" style="width: 19px; height: 33px; top: 0px; left: 0px;">`, */
-          content: `<?xml version="1.0"  standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg class='markerImg'  data=${JSON.stringify(item)} t="1599031324025" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="888" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40"><defs><style type="text/css"></style></defs><path d="M113.777778 387.470222C113.777778 601.457778 512 1024 512 1024s398.222222-422.542222 398.222222-636.529778S731.932444 0 512 0 113.777778 173.482667 113.777778 387.470222zM512 580.266667c-105.187556 0-190.464-84.053333-190.464-187.733334 0-103.68 85.276444-187.733333 190.464-187.733333 105.187556 0 190.464 84.053333 190.464 187.733333 0 103.68-85.276444 187.733333-190.464 187.733334z" p-id="889"></path><path d="M512 398.222222m-113.777778 0a113.777778 113.777778 0 1 0 227.555556 0 113.777778 113.777778 0 1 0-227.555556 0Z" p-id="890"></path></svg>` })
-      })
-    }, 2000)
   },
   methods: {
     watchClick(e) {
@@ -339,6 +281,8 @@ export default {
         if (item.className === 'amap-marker-content') {
           item.childNodes[1].classList.add('markerClickImg')
           this.form = JSON.parse(item.childNodes[1].attributes[1].nodeValue)
+          this.form.createTime = moment(this.form.createTime).format('YYYY-MM-DD HH:mm:SS')
+          // item.createTime = moment(item.createTime).format('YYYY-MM-DD HH:mm:SS')
           this.showZwMes = false
         }
       })
@@ -400,6 +344,28 @@ export default {
     },
     markerClick() {
       console.log('哈哈哈')
+    },
+    getList() {
+      const params = {
+        cascade: true,
+        page: {
+          index: 1,
+          size: 20
+        },
+        params: {
+        }
+      }
+      fetchAllCameraList(params).then(res => {
+        this.formInfo = res.body.data
+        /*  this.formInfo.forEach(item => {
+          item.createTime = moment(item.createTime).format('YYYY-MM-DD HH:mm:SS')
+        }) */
+        this.formInfo.forEach(item => {
+          this.markers.push({ position: [item.longitude, item.latitude], /* content: `<img class='markerImg' data=${JSON.stringify(item)} src="https://webapi.amap.com/theme/v1.3/markers/b/mark_bs.png" style="width: 19px; height: 33px; top: 0px; left: 0px;">`, */
+            content: `<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg  class='markerImg'  data=${JSON.stringify(item)}  t="1599121043094" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2907" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40"><defs><style type="text/css"></style></defs><path d="M512.575 66.562c90.534 0 172.507 36.713 231.841 96.047 59.349 59.334 96.046 141.306 96.046 231.841 0 90.551-36.696 172.522-96.046 231.856-59.334 59.349-141.307 96.047-231.841 96.047-90.535 0-172.522-36.698-231.856-96.047C221.383 566.972 184.687 485 184.687 394.45c0-90.536 36.696-172.507 96.032-231.841 59.333-59.334 141.32-96.047 231.856-96.047zM441.27 439.874c16.993-53.202 41.838-91.409 97.927-125.07-60.031-17.437-129.499 48.742-97.927 125.07z m130.284 319.798v53.364l204.863 36.253v109.068H258.999V849.289l194.611-36.253v-53.349a267.622 267.622 0 0 0 58.965 6.563c20.266 0 40-2.282 58.979-6.578z m-58.979-515.121c-41.408 0-78.891 16.785-106.002 43.896-27.127 27.142-43.913 64.624-43.913 106.002 0 41.393 16.786 78.891 43.913 106.017 27.112 27.112 64.594 43.898 106.002 43.898 41.393 0 78.875-16.786 106.002-43.898 27.127-27.127 43.896-64.624 43.896-106.017 0-41.378-16.77-78.86-43.896-106.002-27.127-27.111-64.609-43.896-106.002-43.896z m73.348 76.564c-18.771-18.771-44.711-30.385-73.349-30.385-28.653 0-54.58 11.615-73.35 30.385-18.771 18.757-30.385 44.697-30.385 73.335 0 28.653 11.615 54.58 30.385 73.365 18.771 18.755 44.697 30.385 73.35 30.385 28.638 0 54.578-11.63 73.349-30.385 18.771-18.786 30.372-44.713 30.372-73.365 0-28.638-11.601-54.578-30.372-73.335z m71.424-71.439c-37.038-37.038-88.239-59.956-144.772-59.956-56.55 0-107.751 22.918-144.789 59.956-37.053 37.053-59.956 88.24-59.956 144.774 0 56.55 22.903 107.751 59.956 144.789 37.038 37.051 88.239 59.971 144.789 59.971 56.534 0 107.735-22.92 144.772-59.971C694.4 502.201 717.32 451 717.32 394.45c0-56.534-22.92-107.721-59.973-144.774z" p-id="2908"></path></svg>`
+          })
+        })
+      })
     }
   }
 }
@@ -410,14 +376,6 @@ export default {
    input {
     font-size: 12px !important;
    }
-  /*  .el-input {
-   font-size: 12px !important;
-
-   }
- .el-input__inner {
-   font-size: 12px !important;
- } */
-
  }
 .main-container {
   height: 100%;
@@ -489,13 +447,20 @@ export default {
          height: 50px;
        }
        .el-form {
-         padding: 20px;
+         padding: 5px;
+         font-size: 12px !important;
        }
        .el-form-item__label {
          font-weight: 500 !important;
+         font-size: 12px !important;
+         padding-right: 3px;
        }
        .el-form-item {
          margin-bottom: 10px;
+         font-size: 12px !important;
+       }
+       .el-form-item__content {
+         font-size: 12px !important;
        }
      }
    }
