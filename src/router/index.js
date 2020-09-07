a5fe24ad39d15b797cdded603cce1ef609d57d5a
v-lqiongyao.gd <v-lqiongyao.gd@chinatelecom.cn>
2020-09-07 20:02:30 +0800

    '查看地图'

--- src/router/index.js
+++ src/router/index.js
@@ -144,48 +144,48 @@ const whiteList = ['/login'] // no redirect whitelist
 //     }
 //   })
 
-  const token = Cookies.get('token')
-  if (token) {
-    // console.log(to.path, store.getters.level)
-    if (to.path === '/login') {
-      next({
-        path: '/'
-      })
-      NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
-    } else {
-      if (!store.getters.level) {
-        store.dispatch('GetUserInfo').then(res => { // 拉取user_info
-          const level = res.data.permissions.level // note: roles must be a array! such as: ['editor','develop']
-          store.dispatch('GenerateRoutes', {
-            level
-          }).then(() => { // 根据roles权限生成可访问的路由表
-            router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
-            next({
-              ...to,
-              replace: true
-            }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
-            return
-          })
-        }).catch(() => {
-          // store.dispatch('FedLogOut').then(() => {
-          //   Message.error('您暂时未有分配的权限，请联系管理员！')
-          //   next('/login');
-          // })
+  // const token = Cookies.get('token')
+  // if (token) {
+  //   // console.log(to.path, store.getters.level)
+  //   if (to.path === '/login') {
+  //     next({
+  //       path: '/'
+  //     })
+  //     NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+  //   } else {
+  //     if (!store.getters.level) {
+  //       store.dispatch('GetUserInfo').then(res => { // 拉取user_info
+  //         const level = res.data.permissions.level // note: roles must be a array! such as: ['editor','develop']
+  //         store.dispatch('GenerateRoutes', {
+  //           level
+  //         }).then(() => { // 根据roles权限生成可访问的路由表
+  //           router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+  //           next({
+  //             ...to,
+  //             replace: true
+  //           }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+  //           return
+  //         })
+  //       }).catch(() => {
+  //         // store.dispatch('FedLogOut').then(() => {
+  //         //   Message.error('您暂时未有分配的权限，请联系管理员！')
+  //         //   next('/login');
+  //         // })
 
-          if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
-            next()
-          } else {
-            store.dispatch('FedLogOut').then(() => {
-              Message.error('您暂时未有分配的权限，请联系管理员！')
-              next('/login');
-            })
-          }
-        })
-      } else {
-        next()
-      }
-    }
-  }
+  //         if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
+  //           next()
+  //         } else {
+  //           store.dispatch('FedLogOut').then(() => {
+  //             Message.error('您暂时未有分配的权限，请联系管理员！')
+  //             next('/login');
+  //           })
+  //         }
+  //       })
+  //     } else {
+  //       next()
+  //     }
+  //   }
+  // }
 
 
 // const whiteList = ['/login'] // no redirect whitelist
