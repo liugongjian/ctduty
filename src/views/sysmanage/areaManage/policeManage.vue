35e2b17e5e677c19cf7e875b92d5fc4e14b211d7
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-09-11 17:50:20 +0800

    路由配置

--- src/views/sysmanage/areaManage/policeManage.vue
+++ src/views/sysmanage/areaManage/policeManage.vue
@@ -3,19 +3,18 @@
   <div class="userManage">
     <el-divider></el-divider>
     <el-row>
-      <el-button class="addbtn" type="warning" @click="addUserDialogVisible=true">+新增用户</el-button>
-      <el-input v-model="queryName" class="searchinput" placeholder="请输入用户姓名"></el-input>
+      <el-button class="addbtn" type="warning" @click="addUserDialogVisible=true">+新增派出所</el-button>
+      <el-input v-model="queryName" class="searchinput" placeholder="请输入派出所姓名"></el-input>
       <el-button class="searchbtn" type="warning" @click="getUserList">搜索</el-button>
       <el-button class="searchbtn" @click="resetQuery">重置</el-button>
     </el-row>
 
     <el-table :data="userList" :header-cell-style="{background:'#ecedee',color:'#717171'}">
-      <el-table-column label="用户名" prop="username"></el-table-column>
-      <el-table-column label="姓名" prop="name"></el-table-column>
-      <el-table-column label="手机号码" prop="phone"></el-table-column>
-      <el-table-column label="岗位" prop="post.name"></el-table-column>
+      <el-table-column label="ID" prop="username"></el-table-column>
+      <el-table-column label="派出所名称" prop="name"></el-table-column>
+      <el-table-column label="所在经度" prop="phone"></el-table-column>
+      <el-table-column label="所在纬度" prop="post.name"></el-table-column>
       <el-table-column label="区域/部门" prop="department.name"></el-table-column>
-      <el-table-column label="权限" prop="permissions.name"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="row_data">
           <el-link type="primary" @click="showEditDialog(row_data.row.id)">编辑</el-link>
@@ -36,100 +35,39 @@
 
     <el-dialog
       :visible.sync="addUserDialogVisible"
-      title="新增用户"
+      title="新增派出所"
       width="50%"
       @close="addDialogClosed">
       <el-form ref="addFormRef" :model="addUserForm" :rules="addUserFormRules" label-width="100px">
-        <el-form-item label="用户名" prop="username">
+        <el-form-item label="派出所名称" prop="username">
           <el-input v-model="addUserForm.username" type="text"></el-input>
         </el-form-item>
-        <el-form-item label="姓名" prop="username">
-          <el-input v-model="addUserForm.name" type="text"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="addUserForm.password" type="password"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号" prop="phone">
-          <el-input v-model="addUserForm.phone" type="text"></el-input>
-        </el-form-item>
-        <el-form-item label="区域/部门">
-          <el-select v-model="addUserForm.departmentId" placeholder="请选择区域/部门">
-            <el-option v-for="item in departmentInfo" :value="item.departmentId" :label="item.department" :key="item.departmentId"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="岗位">
-          <el-select v-model="addUserForm.postId" placeholder="请选择岗位">
-            <el-option v-for="item in postInfo" :value="item.postId" :label="item.post" :key="item.postId"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="权限" prop="permissionId">
-          <el-radio-group v-model="addUserForm.permissionId">
-            <el-radio :label="3274944196083712">系统管理员</el-radio>
-            <el-radio :label="3274944196083713">管理员</el-radio>
-            <el-radio :label="3274944196083714">普通用户</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <!-- <el-form-item label="备注">
-                    <el-input v-model="addUserForm.des" type="textarea"></el-input>
-                </el-form-item> -->
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button type="warning" @click="addAUser">确 定</el-button>
         <el-button @click="addUserDialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
-
     <el-dialog
       :visible.sync="editUserDialogVisible"
-      title="修改用户"
+      title="编辑派出所"
       width="50%"
       @close="editDialogClosed">
       <el-form ref="editFormRef" :rules="addUserFormRules" :model="editUserForm" label-width="100px">
-        <el-form-item label="用户名" prop="username">
+        <el-form-item label="派出所名称" prop="username">
           <el-input v-model="editUserForm.username" type="text"></el-input>
         </el-form-item>
-        <el-form-item label="姓名" prop="username">
-          <el-input v-model="editUserForm.name" type="text"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="editUserForm.password" type="password"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号" prop="phone">
-          <el-input v-model="editUserForm.phone" type="text"></el-input>
-        </el-form-item>
-        <el-form-item label="区域/部门">
-          <!-- <el-select v-model="editUserForm.departmentId" :value="()=>{departmentInfo.find(item => item.departmentId == editUserForm.departmentId)}" placeholder="请选择区域/部门"> -->
-          <el-select v-model="editUserForm.departmentId" :value="editUserForm.departmentId" placeholder="请选择区域/部门">
-            <el-option v-for="item in this.departmentInfo" :value="item.departmentId" :label="item.department" :key="item.departmentId"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="岗位">
-          <el-select v-model="editUserForm.postId" :value="editUserForm.postId" placeholder="请选择岗位">
-            <el-option v-for="item in this.postInfo" :value="item.postId" :label="item.post" :key="item.postId"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="权限" prop="permissionId">
-          <el-radio-group v-model="editUserForm.permissionId">
-            <el-radio :label="3274944196083712">系统管理员</el-radio>
-            <el-radio :label="3274944196083713">管理员</el-radio>
-            <el-radio :label="3274944196083714">普通用户</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <!-- <el-form-item label="备注">
-                    <el-input v-model="addUserForm.des" type="textarea"></el-input>
-                </el-form-item> -->
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button type="warning" @click="editAUser">确 定</el-button>
         <el-button @click="editUserDialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
-
     <el-dialog
       :visible.sync="deleteUserDialogVisible"
-      title="删除用户"
+      title="删除派出所"
       width="50%">
-      <span>确认删除用户{{ this.deleteUserName }}？</span>
+      <span>确认删除派出所{{ deleteUserName }}？</span>
       <span slot="footer" class="dialog-footer">
         <el-button type="warning" @click="deleteAUser">确 定</el-button>
         <el-button @click="deleteUserDialogVisible = false">取 消</el-button>
@@ -154,8 +92,8 @@ export default {
       addUserDialogVisible: false,
       addUserFormRules: {
         username: [
-          { required: true, message: '用户名称不能为空', trigger: 'blur' },
-          { min: 5, max: 10, message: '用户名长度在5-12个字符之间', trigger: 'blur' }
+          { required: true, message: '派出所名称不能为空', trigger: 'blur' },
+          { min: 5, max: 10, message: '派出所名长度在5-12个字符之间', trigger: 'blur' }
         ],
         password: [
           {
@@ -303,8 +241,8 @@ export default {
         console.log(query)
         postAddUser(query).then(response => {
           console.log(response)
-          if (response.code !== 0) return this.$message.error('添加用户失败，请联系系统管理员')
-          this.$message.success('添加用户成功')
+          if (response.code !== 0) return this.$message.error('添加派出所失败，请联系系统管理员')
+          this.$message.success('添加派出所成功')
           this.addUserDialogVisible = false
           this.getUserList()
         })
@@ -317,7 +255,7 @@ export default {
     showEditDialog(id) {
       const { data: res } = getUserInfo(id).then(response => {
         // console.log(response)
-        if (response.code !== 0) return this.$message.error('获取用户信息失败')
+        if (response.code !== 0) return this.$message.error('获取派出所信息失败')
         this.editUserForm = response.body.data
         this.editUserDialogVisible = true
       })
@@ -327,10 +265,10 @@ export default {
         if (!valid) return
         updateUser([{ ...this.editUserForm }]).then(response => {
           // console.log(response)
-          if (response.code !== 0) return this.$message.error('更新用户信息失败,请稍后再试')
+          if (response.code !== 0) return this.$message.error('更新派出所信息失败,请稍后再试')
           this.editUserDialogVisible = false
           this.getUserList()
-          this.$message.success('更新用户信息成功')
+          this.$message.success('更新派出所信息成功')
         })
       })
     },
@@ -347,12 +285,12 @@ export default {
       const ids = []
       ids.push(this.deleteUserId)
       deleteUser(ids).then(response => {
-        if (response.code !== 0) return this.$message.error('删除用户失败,请稍后再试')
+        if (response.code !== 0) return this.$message.error('删除派出所失败,请稍后再试')
         this.deleteUserDialogVisible = false
         this.deleteUserId = 0
         this.deleteUserName = ''
         this.getUserList()
-        this.$message.success('删除用户信息')
+        this.$message.success('删除派出所信息')
       })
     },
 
