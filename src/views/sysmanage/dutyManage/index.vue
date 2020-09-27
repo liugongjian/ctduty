1920b3618eb52d619cbdbe9802b09fae0e294307
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-09-27 19:26:59 +0800

    接口联调完毕

--- src/views/sysmanage/dutyManage/index.vue
+++ src/views/sysmanage/dutyManage/index.vue
@@ -22,7 +22,11 @@
               tooltip-effect="dark"
               fit
             >
-              <el-table-column :show-overflow-tooltip="true" :label="'值班日期'" prop="scheduleDate"></el-table-column>
+              <el-table-column :show-overflow-tooltip="true" :label="'值班日期'" prop="scheduleDate">
+                <template slot-scope="scope">
+                  <span>{{ renderTime(scope.row.scheduleDate) }}</span>
+                </template>
+              </el-table-column>
               <el-table-column :show-overflow-tooltip="true" :label="'值班时间'" prop="type">
                 <template slot-scope="scope">
                   <span>{{ scope.row.startTime }} - {{ scope.row.endTime }}</span>
@@ -71,7 +75,11 @@
         tooltip-effect="dark"
         fit
       >
-        <el-table-column :show-overflow-tooltip="true" :label="'值班日期'" prop="scheduleDate"></el-table-column>
+        <el-table-column :show-overflow-tooltip="true" :label="'值班日期'" prop="scheduleDate">
+          <template slot-scope="scope">
+            <span>{{ renderTime(scope.row.scheduleDate) }}</span>
+          </template>
+        </el-table-column>
         <el-table-column :show-overflow-tooltip="true" :label="'值班时间'" prop="type">
           <template slot-scope="scope">
             <span>{{ scope.row.startTime }} - {{ scope.row.endTime }}</span>
@@ -93,6 +101,7 @@
 <script>
 import { Message } from 'element-ui'
 import Cookies from 'js-cookie'
+import { renderTime } from '@/utils'
 import Pagination from '@/components/Pagination'
 import 'element-ui/lib/theme-chalk/index.css'
 import moment from 'moment'
@@ -109,6 +118,7 @@ export default {
   components: { Pagination },
   data() {
     return {
+      renderTime,
       path: 'http://host31.880508.xyz:10000/Schedule/Template',
       importHeader: {
         Authorization: token
