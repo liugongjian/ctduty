202ab8922b10afd9c2120b57839e02bc821a5c7c
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-10-12 09:45:16 +0800

    首页echarts高度自适应

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -204,9 +204,7 @@ export default {
     resize() { // 当宽高变化时就会执行
       // 执行某些操作
       [].forEach.call(document.getElementsByTagName('canvas'), function(item) {
-        console.log(item.parentNode.parentNode.id)
         if (item.parentNode.parentNode.id === 'man' || item.parentNode.parentNode.id === 'car' || item.parentNode.parentNode.id === 'bicycle') {
-          console.log(item.parentNode.parentNode.parentNode)
           item.style.width = item.parentNode.parentNode.parentNode.clientWidth / 3 + 'px'
           item.parentNode.style.width = item.parentNode.parentNode.parentNode.clientWidth / 3 + 'px'
         } else {
