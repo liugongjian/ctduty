ac2934522b8e085cd06d5f4a9a8d351909efed95
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-10-20 15:58:27 +0800

    表格样式调整

--- src/api/camera.js
+++ src/api/camera.js
@@ -50,3 +50,20 @@ export function delCamera(query) {
   })
 }
 
+// 算法列表
+export function taskList(query) {
+  return request({
+    url: '/nvsapi/task/QueryActive',
+    method: 'post',
+    data: query
+  })
+}
+
+// 新增算法
+export function addTask(query) {
+  return request({
+    url: '/nvsapi/taskInst/add',
+    method: 'post',
+    data: query
+  })
+}
