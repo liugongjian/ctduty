9f392186e0a0f346349797763bc1fc124fa3b99c
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-10-23 10:27:05 +0800

    监控大屏改造

--- src/views/alarmMessage/index.vue
+++ src/views/alarmMessage/index.vue
@@ -98,7 +98,7 @@
               <!--  <el-table-column :show-overflow-tooltip="true" :label="'摄像头'" min-width="15%" prop="camera.address"></el-table-column> -->
               <el-table-column :label="'图片'" min-width="20%">
                 <template slot-scope="scope">
-                  <el-image :src="scope.row.imageCompress" class="image" @click="openBig(scope.row.image)" />
+                  <el-image :src="scope.row.imageCompress" style="width:170px; height:97px;" @click="openBig(scope.row.image)" />
                 </template>
               </el-table-column>
               <el-table-column :show-overflow-tooltip="true" :label="'处理人'" min-width="5%" prop="handler.username" width="100">
@@ -597,39 +597,30 @@ export default {
 }
 </script>
 
-    <style lang='scss'>
-    .image {
-      width: 100%;
-      max-width: 250px;
-      height:100px;
-      img{
-        object-fit: contain;//cover;
-        background-color: rgb(245, 247, 250);
-      }
-    }
-    .title {
-     width: 100%;
-     height: 50px;
-     line-height: 50px;
-     font-family: MicrosoftYaHei;
-     font-size: 22px;
-     color: #333333;
-     font-weight: 500;
-     border-bottom: 1px solid #ccc;
-     background: #FFF;
-     padding: 0 20px;
-   }
-   .el-date-editor{
-     height: 28px !important;
-   }
-   .el-range-separator{
+<style lang='scss'>
+ .title {
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    font-family: MicrosoftYaHei;
+    font-size: 22px;
+    color: #333333;
+    font-weight: 500;
+    border-bottom: 1px solid #ccc;
+    background: #FFF;
+    padding: 0 20px;
+  }
+  .el-date-editor{
+    height: 28px !important;
+  }
+  .el-range-separator{
 
-     width: 30px !important;
-   }
-   .el-select-dropdown__item {
-       font-size: 12px !important;
+    width: 30px !important;
+  }
+  .el-select-dropdown__item {
+      font-size: 12px !important;
 
-   }
+  }
   .deal {
     fill: #44bd32 !important;
   }
