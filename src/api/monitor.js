545cef7ddca512a3d8dac38e20c71f6d3931b8ba
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-11-05 14:17:13 +0800

    vuex告警提示音

--- src/api/monitor.js
+++ src/api/monitor.js
@@ -1,54 +1,54 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
 
 export function fetchAllMonitor(query) {
   return request({
-    url: "/nvsapi/camera/live/query",
-    method: "get",
+    url: '/nvsapi/camera/live/query',
+    method: 'get',
     data: query
-  });
+  })
 }
 
 export function updateMonitor(data) {
   return request({
     url: `/nvsapi/camera/live/update/${data.id}/${data.cameraId}`,
-    method: "put",
+    method: 'put',
     data: data
-  });
+  })
 }
 
 export function addMonitor(data) {
   return request({
     url: `/nvsapi/camera/live/add/${data.cameraId}`,
-    method: "post",
+    method: 'post',
     data: data
-  });
+  })
 }
 
 export function delMonitor(id) {
   return request({
     url: `/nvsapi/camera/live/delete/${id}`,
-    method: "delete"
-  });
+    method: 'delete'
+  })
 }
 
 export function play(cameraId) {
   return request({
     url: `/nvsapi/camera/live/play/${cameraId}`,
-    method: "post"
-  });
+    method: 'post'
+  })
 }
 
 export function stop(id) {
   return request({
     url: `/nvsapi/camera/live/stop/${id}`,
-    method: "delete"
-  });
+    method: 'delete'
+  })
 }
 
 export function loadingImg() {
   return request({
     url: `/nvsapi/camera/live/image`,
     // url: `http://192.168.50.152:10001/camera/live/image`,
-    method: "get"
-  });
+    method: 'get'
+  })
 }
