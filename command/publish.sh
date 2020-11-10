18d9d4f67b8dbf426605d6a14d2040401438befc
rejiu <zhaodan@chinatelecom.cn>
2020-11-10 09:42:24 +0800

    告警信息页面  告警类型添加

--- command/publish.sh
+++ command/publish.sh
@@ -4,4 +4,4 @@ npm run build:prod && echo "=============================打包完成===========
 echo "=============================压缩文件============================="
 tar -zvcf dist.tar.gz dist
 echo "=============================开始上传============================="
-scp dist.tar.gz root@36.41.71.26:/opt/ctyun-ai-duty-pc
+scp dist.tar.gz root@59.36.77.57:/opt//ningxia-video-admin
