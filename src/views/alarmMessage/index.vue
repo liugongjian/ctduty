4a5681bbfe92116f08516fcecdb722aa21e34f9f
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-11-18 17:44:53 +0800

    设备名称bug

--- src/views/alarmMessage/index.vue
+++ src/views/alarmMessage/index.vue
@@ -92,7 +92,7 @@
             <el-option
               v-for="item in typeOptions"
               :key="item._id"
-              :label="item.address"
+              :label="item.name"
               :value="item._id"
             ></el-option>
           </el-select>
