87475b3573330c0456713de222df726e4aea051e
Miro <ak_._47@qq.com>
2020-11-19 18:36:54 +0800

    视频监控

--- src/views/videoMonitor/index.vue
+++ src/views/videoMonitor/index.vue
@@ -6,8 +6,14 @@
           <div class="monitorBox">
             <div class="video-panel">
               <div class="left-part">
-                <VideoPlayer v-if="!!flv && activeTab == 'video'" :video-ref="videoRef" :options="videoOptions"/>
+                <VideoPlayer
+                  :video-ref="cameraId"
+                  :key="cameraId"
+                  :options="videoOptions"
+                />
               </div>
+              <!-- v-if="!!flv && activeTab == 'video'" -->
+              <!-- :video-ref="videoRef" -->
               <div class="right-part">
                 <div class="video-control">
                   <div class="control-box">
@@ -89,91 +95,114 @@
                 </el-table-column>
               </el-table>
             </div>
-            <div class="realTimeData">
-              <div class="panelTitle">实时分析</div>
-              <div class="streamData">
-                <div class="dataHeader">人流分析</div>
-                <div class="dataTitle">
-                  今日累计
-                </div>
-                <div class="dataText">
-                  <div class="dataShow displayIB">
-                    <p>流入</p>
-                    <div>18人</div>
-                  </div>
-                  <div class="dataShow displayIB">
-                    <p>流出</p>
-                    <div>12人</div>
-                  </div>
-                </div>
-                <div class="dataTitle">
-                  实时流量（近一小时）
-                </div>
-                <div class="dataText">
-                  <div class="dataShow displayIB">
-                    <p>流入</p>
-                    <div>18人</div>
-                  </div>
-                  <div class="dataShow displayIB">
-                    <p>流出</p>
-                    <div>12人</div>
-                  </div>
-                </div>
-              </div>
-              <!-- <div class="streamData">
-                <div class="dataHeader">车流分析</div>
-                <div class="dataTitle">
-                  今日累计
-                </div>
-                <div class="dataText">
-                  <div class="dataShow displayIB">
-                    <p>流入</p>
-                    <div>18人</div>
-                  </div>
-                  <div class="dataShow displayIB">
-                    <p>流出</p>
-                    <div>12人</div>
-                  </div>
-                </div>
-                <div class="dataTitle">
-                  实时流量（近一小时）
-                </div>
-                <div class="dataText">
-                  <div class="dataShow displayIB">
-                    <p>流入</p>
-                    <div>18人</div>
-                  </div>
-                  <div class="dataShow displayIB">
-                    <p>流出</p>
-                    <div>12人</div>
-                  </div>
-                </div>
-              </div> -->
-            </div>
           </div>
         </div>
 
       </div>
-      <div class="photoBox">
-        <div class="showPhoto">
-          <span>实时抓拍</span>
-          <el-select class="photo-fitler-sel" size="mini"></el-select>
-        </div>
-        <div class="photoContainer">
-          <div v-for="(o, index) in 10" :key="o" class="photoList">
-            <el-card :body-style="{ padding: '0px' }">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" style="width: 100%; height:120px">
-              <div style="padding: 14px;">
-                <span>人员</span>
-                <div>2020-08-31  23:00:00 </div>
+      <div class="rightPanel">
+        <div class="realTimeData">
+          <!-- <div class="panelTitle">实时分析</div> -->
+          <div class="streamData-wrapper">
+            <div class="streamData">
+              <div class="dataHeader">人流分析</div>
+              <div class="dataTitle">
+                今日累计
               </div>
-            </el-card>
+              <div class="dataText">
+                <div class="dataShow displayIB">
+                  <p>流入</p>
+                  <div>0人</div>
+                </div>
+                <div class="dataShow displayIB">
+                  <p>流出</p>
+                  <div>0人</div>
+                </div>
+              </div>
+              <div class="dataTitle">
+                实时流量（近一小时）
+              </div>
+              <div class="dataText">
+                <div class="dataShow displayIB">
+                  <p>流入</p>
+                  <div>0人</div>
+                </div>
+                <div class="dataShow displayIB">
+                  <p>流出</p>
+                  <div>0人</div>
+                </div>
+              </div>
+            </div>
+            <div class="streamData">
+              <div class="dataHeader">车流分析</div>
+              <div class="dataTitle">
+                今日累计
+              </div>
+              <div class="dataText">
+                <div class="dataShow displayIB">
+                  <p>流入</p>
+                  <div>0辆</div>
+                </div>
+                <div class="dataShow displayIB">
+                  <p>流出</p>
+                  <div>0辆</div>
+                </div>
+              </div>
+              <div class="dataTitle">
+                实时流量（近一小时）
+              </div>
+              <div class="dataText">
+                <div class="dataShow displayIB">
+                  <p>流入</p>
+                  <div>0辆</div>
+                </div>
+                <div class="dataShow displayIB">
+                  <p>流出</p>
+                  <div>0辆</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="photoBox">
+          <div class="showPhoto">
+            <span>今日实时抓拍</span>
+            <el-select
+              v-model="taskId"
+              collapse-tags
+              clearable
+              class="photo-fitler-sel"
+              size="mini"
+              multiple
+            >
+              <el-option
+                v-for="item in taskData"
+                :key="item.id"
+                :label="item.cnName"
+                :value="item.id">
+              </el-option>
+            </el-select>
+          </div>
+          <div v-loading="photosLoading" v-if="photoCardList && photoCardList.length > 0" class="photoContainer" >
+            <div v-for="(item) in photoCardList" :key="item.id" class="photoList">
+              <div v-if="item.myPlaceholder" style="opacity:0"></div>
+              <el-card v-else :body-style="{ padding: '0px' }">
+                <el-image :src="item.imageCut" style="width: 100%; height:120px;" class="photoList-image" />
+                <div style="padding: 14px;">
+                  <span>{{ getTaskById(item.taskId) }}</span>
+                  <div>{{ getDateTimeStr(item.createTime) }}</div>
+                </div>
+              </el-card>
+            </div>
+          </div>
+          <div v-else class="photoContainer photoContainer-noData">
+            <div class="photoContainer-noData-text">暂无数据</div>
           </div>
           <pagination
             v-show="total>0"
             :total="total"
             :page.sync="page"
             :limit.sync="limit"
+            small
             @pagination="pageChange()"
           />
         </div>
@@ -183,16 +212,44 @@
 </template>
 
 <script>
+import moment from 'moment'
 import Pagination from '@/components/Pagination'
 import VideoPlayer from '@/components/VideoPlayer'
+import { getAlertInfos } from '@/api/alarm'
+import { taskList } from '@/api/algorithm'
+const dateTimeFormat = 'YYYY-MM-DD HH:mm:ss'
+const timeFormat = 'MM-DD HH:mm:ss'
 export default {
   components: { Pagination, VideoPlayer },
   data() {
     return {
+      cameraId: '61010010001320014342',
+      taskId: '',
+      taskData: [],
+      photosLoading: false,
+      photoCardList: [],
       pageLoading: false,
       total: 10,
       page: 0,
       showVideoSetting: false,
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        width: 400, // 播放器宽度
+        height: 300, // 播放器高度
+        // poster: 'http://www.jq22.com/demo/vide7.1.0201807161136/m.jpg',
+        fluid: true, // 流体布局，自动充满，并保持播放其比例
+        sources: [
+          {
+            src: item.rtmpuri ? item.rtmpuri + '&a.flv' : '',
+            type: this.video_type(item.rtmpuri ? item.rtmpuri + '&a.flv' : '')
+          }
+          // {
+          //   src: item.rtmpuri,
+          //   type: 'application/x-mpegURL'
+          // }
+        ]
+      },
       tableData: [{
         date: '2222',
         name: '111',
@@ -212,9 +269,94 @@ export default {
       }]
     }
   },
+  watch: {
+    taskId(taskId) {
+      this.getPhotoList(taskId)
+    }
+  },
+  async created() {
+    await this.getTaskList()
+    await this.getPhotoList()
+  },
   methods: {
     sureThis() {
       this.showVideoSetting = true
+    },
+    getTaskById(id) {
+      const target = this.taskData.find(item => item.id === id)
+      return target.cnName
+    },
+    getTaskList() {
+      const query = {
+        cascade: true,
+        page: {
+          index: 1,
+          size: 100
+        },
+        params: {}
+      }
+      taskList(query).then(res => {
+        if (res.code === 0) {
+          this.taskData = res.body.data
+          //   this.algorithmId = res.body.data[0].id
+          // this. = false
+        } else {
+          this.taskData = []
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    pageChange(e) {
+      console.log('change')
+      //   this.currentTab = e.label
+      //   const s = e.label + ' ' + this.startTime + ':00'
+      //   const end = e.label + ' ' + this.endTime + ':00'
+      //   const h = this.formInline.typeValue
+      //   this.page = 1
+      this.getPhotoList()
+    },
+    getDateTimeStr(time) {
+      return moment(time).format(timeFormat)
+    },
+    getPhotoList() {
+      this.photosLoading = true
+      const param = [{
+        field: 'createTime',
+        operator: 'BETWEEN',
+        value: { 'start': moment().startOf('day').format(dateTimeFormat), 'end': moment().endOf('day').format(dateTimeFormat) }
+      }
+      ]
+      if (this.taskId && this.taskId.length) {
+        param.push({
+          field: 'taskId',
+          operator: 'IN',
+          value: [...this.taskId]
+        })
+      }
+      const params = {
+        cascade: true,
+        page: {
+          index: this.page,
+          size: this.limit
+        },
+        params: param,
+        sorts: [
+          {
+            field: 'create_Time',
+            type: 'desc'
+          }
+        ]
+      }
+      getAlertInfos(params).then(res => {
+        const { body: { data, page: { total }}} = res
+        this.photoCardList = data
+        this.total = total
+        this.photosLoading = false
+      }).catch(err => {
+        console.log(err)
+        this.photosLoading = false
+      })
     }
   }
 }
@@ -235,6 +377,7 @@ export default {
     display: flex;
     height:100%;
     .leftPanel{
+      width:50%;
       flex-grow: 1;
       .leftContent{
         display: flex;
@@ -258,11 +401,71 @@ export default {
         }
       }
     }
-    .photoBox{
-      background: #fff;
-      width:420px;
+    .rightPanel{
+      width:50%;
       height:100%;
-      margin-left: 20px;
+      flex-grow: 1;
+      display:flex;
+      flex-direction: column;
+      .realTimeData{
+        // flex-grow: 1;
+        // width:50%;
+        height:210px;
+        margin-left: 20px;
+        // background: #ffffff;
+        // padding:20px;
+        .streamData-wrapper{
+          display:flex;
+          justify-content: space-between;
+        }
+        .streamData{
+          width: 48%;
+          background: #ffffff;
+          border: 1px solid #F0EFEF;
+          padding-bottom: 15px;
+        }
+        .dataHeader{
+            height: 30px;
+            line-height: 30px;
+            // font-size: 14px;
+            // background: #F0EFEF;
+            padding-left: 18px;
+            color:rgba(0,0,0,0.85);
+            // 注意回退
+             padding-top:10px;
+             font-size:16px;
+        }
+        .dataTitle{
+            font-size: 14px;
+            margin-top: 15px;
+            margin-left: 20px;
+        }
+        .dataText{
+            margin-left: 20px;
+        }
+        .dataShow{
+          width:100px;
+            p{
+                font-size: 12px;
+                color:rgba(0,0,0,0.85);
+                margin: 16px 0 7px;
+            }
+            div{
+                font-size: 14px;
+            }
+        }
+      }
+    }
+    .photoBox{
+      flex-grow:1;
+      flex-shrink: 1;
+      position:relative;
+      background: #fff;
+      width:calc(100% - 20px);
+      height:calc(100% - 340px);
+      min-height: 200px;
+      margin: 20px 20px 0 20px;
+      // height:100%;
       padding:20px;
       .showPhoto{
         height: 40px;
@@ -270,18 +473,59 @@ export default {
         font-size: 16px;
         position:relative;
         .photo-fitler-sel{
-          width:120px;
+          width:180px;
           position: absolute;
           right:0;
         }
       }
-      .photoList{
-          display: inline-block;
-          width: 20%;
-          text-align: center;
-      }
       .photoContainer{
-        position: relative;
+        display: flex;
+        height: calc(100% - 80px);
+        overflow-y:auto;
+        flex-wrap: wrap;
+        // justify-content: space-between;
+        .photoList-image{
+          object-fit: contain;
+        }
+        @media screen and (max-width: 1400px){
+          .photoList{
+            // display: inline-block;
+            width: 31%;
+            height:170px;
+            margin-bottom: 20px;
+            margin-right: 2%;
+          }
+        }
+        @media screen and (min-width: 1401px) and (max-width: 1500px){
+          .photoList{
+            // display: inline-block;
+            width: 23%;
+            height:170px;
+            margin-bottom: 20px;
+            margin-right: 2%;
+          }
+        }
+         @media screen and (min-width: 1501px){
+          .photoList{
+            // display: inline-block;
+            width: 18%;
+            height:170px;
+            margin-bottom: 20px;
+            margin-right: 2%;
+          }
+        }
+          .photoContainer-noData{
+            &-text{
+              height: 100%;
+              width:100%;
+              line-height: 250px;
+              font-size: 20px;
+              color:#999;
+              text-align: center;
+              vertical-align: middle;
+            }
+          }
+
       }
     }
   }
@@ -301,7 +545,7 @@ export default {
   //    }
   // }
   .censusData{
-    display: flex;
+    // display: flex;
     margin-top: 20px;
     height:300px;
     width: 100%;
@@ -315,62 +559,25 @@ export default {
       table-layout: auto;
     }
     .alertTable{
-      flex-grow: 1;
+      // flex-grow: 1;
       background: #ffffff;
+      height:100%;
       padding:20px;
-      width:50%;
-    }
-    .realTimeData{
-      flex-grow: 1;
-      width:50%;
-      margin-left: 20px;
-      background: #ffffff;
-      padding:20px;
-      .streamData{
-          border: 1px solid #F0EFEF;
-          padding-bottom: 20px;
-      }
-      .dataHeader{
-          height: 36px;
-          line-height: 36px;
-          font-size: 16px;
-          background: #F0EFEF;
-          padding-left: 18px;
-          color:rgba(0,0,0,0.85);
-      }
-      .dataTitle{
-          font-size: 14px;
-          margin-top: 20px;
-          margin-left: 20px;
-      }
-      .dataText{
-          margin-left: 20px;
-      }
-      .dataShow{
-          p{
-              font-size: 12px;
-              color:rgba(0,0,0,0.85);
-              margin: 16px 0 7px;
-          }
-          div{
-              font-size: 14px;
-          }
-      }
+      width:100%;
     }
   }
 
   .pagination-container{
-    // margin-top: 250px;
-    .showTotal{
-      padding-left: 10px;
-    }
+    position: absolute;
+    bottom: 20px;
+    width:calc(100% - 20px);
   }
 //   .video-panel {
 //     position: relative;
 //     height: 100%;
 //   }
   .right-part {
-    width: 30%;
+    width: 200px;
     position: absolute;
     right: 0;
     top: calc(50% - 100px);
