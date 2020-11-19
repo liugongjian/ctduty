4a6faad365b75acb8c117a5ee52ac0979513e28c
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-11-19 14:33:20 +0800

    user接口回退

--- src/views/sysmanage/userManage/index.vue
+++ src/views/sysmanage/userManage/index.vue
@@ -170,25 +170,25 @@
 </template>
 
 <script>
-import Pagination from "@/components/Pagination";
+import Pagination from '@/components/Pagination'
 import {
   fetchUserList,
   postAddUser,
   getUserInfo,
   updateUser,
   deleteUser
-} from "@/api/users";
+} from '@/api/users'
 
 export default {
   components: { Pagination },
   data() {
     var checkMobile = (rule, value, cb) => {
-      const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[5-7])[0-9]{8}$/;
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[5-7])[0-9]{8}$/
       if (regMobile.test(value)) {
-        return cb();
+        return cb()
       }
-      cb(new Error("请输入合法的手机号"));
-    };
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
       page: 1,
       limit: 10,
@@ -196,61 +196,61 @@ export default {
       addUserDialogVisible: false,
       addUserFormRules: {
         username: [
-          { required: true, message: "用户名不能为空", trigger: "blur" },
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
           {
             min: 5,
             max: 10,
-            message: "用户名长度在5-12个字符之间",
-            trigger: "blur"
+            message: '用户名长度在5-12个字符之间',
+            trigger: 'blur'
           }
         ],
         name: [
-          { required: true, message: "姓名不能为空", trigger: "blur" },
+          { required: true, message: '姓名不能为空', trigger: 'blur' },
           {
             min: 5,
             max: 10,
-            message: "姓名长度在5-12个字符之间",
-            trigger: "blur"
+            message: '姓名长度在5-12个字符之间',
+            trigger: 'blur'
           }
         ],
         password: [
           {
             required: true,
-            message: "密码不能为空",
-            trigger: "blur"
+            message: '密码不能为空',
+            trigger: 'blur'
           },
           {
             min: 8,
             max: 20,
-            message: "密码长度在8-20个字符之间",
-            trigger: "blur"
+            message: '密码长度在8-20个字符之间',
+            trigger: 'blur'
           }
         ],
         departmentId: [
           {
             required: true,
-            message: "请选择部门",
-            trigger: "change"
+            message: '请选择部门',
+            trigger: 'change'
           }
         ],
         postId: [
           {
             required: true,
-            message: "请选择岗位",
-            trigger: "change"
+            message: '请选择岗位',
+            trigger: 'change'
           }
         ],
         phone: [
           {
             required: true,
-            message: "手机号不能为空",
-            trigger: "blur"
+            message: '手机号不能为空',
+            trigger: 'blur'
           },
           {
             min: 11,
             max: 11,
-            message: "长度为11个字符",
-            trigger: "blur"
+            message: '长度为11个字符',
+            trigger: 'blur'
           } /* ,
           {
             validator: checkMobile,
@@ -258,90 +258,90 @@ export default {
           } */
         ],
         permissionId: [
-          { required: true, message: "权限不能为空", trigger: "change" }
+          { required: true, message: '权限不能为空', trigger: 'change' }
         ]
       },
       addUserForm: {
-        username: "",
-        name: "",
-        password: "",
-        permissionId: "",
+        username: '',
+        name: '',
+        password: '',
+        permissionId: '',
         departmentId: null,
         postId: null,
-        phone: ""
+        phone: ''
       },
       editUserForm: {
         id: 0,
-        username: "",
-        name: "",
-        password: "",
-        permissionId: "",
+        username: '',
+        name: '',
+        password: '',
+        permissionId: '',
         departmentId: null,
         postId: null,
-        phone: ""
+        phone: ''
       },
       userList: [],
       queryInfo: {
         pagenum: 1,
         pagesize: 10
       },
-      queryName: "",
+      queryName: '',
       total: 0,
       editUserDialogVisible: false,
-      deleteUserName: "",
+      deleteUserName: '',
       deleteUserDialogVisible: false,
       deleteUserId: 0,
       departmentInfo: [
         {
           departmentId: 3275699862611970,
-          department: "华阴公安局"
+          department: '华阴公安局'
         },
         {
           departmentId: 3275699862611971,
-          department: "孟塬派出所"
+          department: '孟塬派出所'
         },
         {
           departmentId: 3275699862611972,
-          department: "华山镇派出所"
+          department: '华山镇派出所'
         }
       ],
 
       postInfo: [
         {
           postId: 3275699862609920,
-          post: "所长"
+          post: '所长'
         },
         {
           postId: 3275699862609921,
-          post: "副所长"
+          post: '副所长'
         },
         {
           postId: 3275699862609922,
-          post: "民警"
+          post: '民警'
         },
         {
           postId: 3275699862609923,
-          post: "普通员工"
+          post: '普通员工'
         },
         {
           postId: 3275699862611968,
-          post: "管控中心"
+          post: '管控中心'
         },
         {
           postId: 3275699862611969,
-          post: "监控中心"
+          post: '监控中心'
         }
       ]
-    };
+    }
   },
   watch: {
     limit() {
-      this.page = 1;
-      this.pageChange();
+      this.page = 1
+      this.pageChange()
     }
   },
   created() {
-    this.getUserList();
+    this.getUserList()
   },
   methods: {
     getUserList() {
@@ -353,75 +353,75 @@ export default {
         },
         params: [
           {
-            field: "name",
-            operator: "LIKE",
+            field: 'name',
+            operator: 'LIKE',
             value: `%${this.queryName.trim()}%`
           }
         ]
-      };
+      }
       fetchUserList(query).then(response => {
-        if (response.code !== 0) return;
-        this.userList = response.body.data;
-        this.total = response.body.page.total;
-      });
+        if (response.code !== 0) return
+        this.userList = response.body.data
+        this.total = response.body.page.total
+      })
     },
     pageChange() {
       if (this.oldSize !== this.limit) {
-        this.page = 1;
+        this.page = 1
       }
-      this.oldSize = this.limit;
-      this.getUserList();
+      this.oldSize = this.limit
+      this.getUserList()
     },
     handleSizeChange(newsize) {
-      this.queryInfo.pagesize = newsize;
-      this.getUserList();
+      this.queryInfo.pagesize = newsize
+      this.getUserList()
     },
     handleCurrentChange(newpage) {
-      this.queryInfo.pagenum = newpage;
-      this.getUserList();
+      this.queryInfo.pagenum = newpage
+      this.getUserList()
     },
     addAUser() {
       this.$refs.addFormRef.validate(valid => {
-        if (!valid) return;
-        const query = [{ ...this.addUserForm }];
+        if (!valid) return
+        const query = [{ ...this.addUserForm }]
         postAddUser(query).then(response => {
           if (response.code !== 0) {
-            return this.$message.error("添加用户失败，请联系系统管理员");
+            return this.$message.error('添加用户失败，请联系系统管理员')
           }
-          this.$message.success("添加用户成功");
-          this.addUserDialogVisible = false;
-          this.getUserList();
-        });
-      });
+          this.$message.success('添加用户成功')
+          this.addUserDialogVisible = false
+          this.getUserList()
+        })
+      })
     },
     addDialogClosed() {
-      this.addUserForm = {};
-      this.$refs.addFormRef.resetFields();
+      this.addUserForm = {}
+      this.$refs.addFormRef.resetFields()
     },
     showEditDialog(id) {
       const { data: res } = getUserInfo(id).then(response => {
         // console.log(response)
-        if (response.code !== 0) return this.$message.error("获取用户信息失败");
-        this.editUserForm = response.body.data;
-        this.editUserDialogVisible = true;
-      });
+        if (response.code !== 0) return this.$message.error('获取用户信息失败')
+        this.editUserForm = response.body.data
+        this.editUserDialogVisible = true
+      })
     },
     editAUser() {
       this.$refs.editFormRef.validate(valid => {
-        if (!valid) return;
+        if (!valid) return
         updateUser([{ ...this.editUserForm }]).then(response => {
           // console.log(response)
           if (response.code !== 0) {
-            return this.$message.error("更新用户信息失败,请稍后再试");
+            return this.$message.error('更新用户信息失败,请稍后再试')
           }
-          this.editUserDialogVisible = false;
-          this.getUserList();
-          this.$message.success("更新用户信息成功");
-        });
-      });
+          this.editUserDialogVisible = false
+          this.getUserList()
+          this.$message.success('更新用户信息成功')
+        })
+      })
     },
     editDialogClosed() {
-      this.editUserForm = {};
+      this.editUserForm = {}
     },
     // showDeleteDialog(username, id) {
     //   this.deleteUserDialogVisible = true
@@ -429,24 +429,24 @@ export default {
     //   this.deleteUserId = id
     // },
     showDeleteDialog(id) {
-      this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
       }).then(() => {
-        const ids = [];
-        ids.push(this.deleteNoticerId);
+        const ids = []
+        ids.push(this.deleteNoticerId)
         deleteUser([id]).then(response => {
           if (response.code !== 0) {
-            return this.$message.error("删除失败,请稍后再试");
+            return this.$message.error('删除失败,请稍后再试')
           }
 
-          this.deleteUserId = 0;
-          this.deleteUserName = "";
-          this.getUserList();
-          this.$message.success("删除信息成功");
-        });
-      });
+          this.deleteUserId = 0
+          this.deleteUserName = ''
+          this.getUserList()
+          this.$message.success('删除信息成功')
+        })
+      })
     },
 
     // deleteAUser() {
@@ -465,11 +465,11 @@ export default {
     // },
 
     resetQuery() {
-      this.queryName = "";
-      this.getUserList();
+      this.queryName = ''
+      this.getUserList()
     }
   }
-};
+}
 </script>
 
 <style lang='scss' scoped>
