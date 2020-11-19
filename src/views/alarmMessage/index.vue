5473a286f3ab49d4ecd68352eee92cb266986d38
v-wangwenw.gd <v-wangwenw.gd@chinatelecom.cn>
2020-11-19 15:49:40 +0800

    识别结果

--- src/views/alarmMessage/index.vue
+++ src/views/alarmMessage/index.vue
@@ -99,10 +99,10 @@
           <br>
           <el-select
             v-model="algorithmList.typeValue"
-            :multiple-limit="2"
             multiple
             placeholder="请选择事件名称"
             min-width="300px"
+            collapse-tags
             @change="checkModel"
           >
             <el-option
@@ -114,10 +114,10 @@
           </el-select>
           <el-select
             v-model="algorithmNameList.typeValue"
-            :multiple-limit="2"
             multiple
             placeholder="请选择算法名称"
             min-width="350px"
+            collapse-tags
             @change="algorithmCheck"
           >
             <el-option
