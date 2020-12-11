0cc2cf1de5c9044405ee5b55a9cf695d1462128c
rejiu <zhaodan@chinatelecom.cn>
2020-12-11 13:54:35 +0800

    天翼值更页面重构

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
 
         // can be overwritten by process.env.HOST
         // if you want dev by ip, please set host: '0.0.0.0'
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
         errorOverlay: true,
